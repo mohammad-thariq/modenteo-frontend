724d49f90a07892c836544ae92e6533c073406ec
Gomathy <gomathy1023@gmail.com>
2024-05-27 21:46:14 +0530

    URL Changes

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,7 +1,7 @@
 
 // const BACKEND_API_URL = "https://modenteo-backend.onrender.com/api.modenteo/v1";
-const BACKEND_API_URL = "http://localhost:3000/api.modenteo/v1";
-const BACKEND_BASE_URL = "http://localhost:3000/";
+const BACKEND_API_URL = "https://api-modenteo.onrender.com/v1";
+const BACKEND_BASE_URL = "https://api-modenteo.onrender.com/";
 // const BASE_URL = "https://dev-modenteo.vercel.app/";
 const BASE_URL= "http://localhost:3001/";
 export { BACKEND_API_URL, BACKEND_BASE_URL, BASE_URL };
