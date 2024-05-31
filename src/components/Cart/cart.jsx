2d6b80507473f1dbbf8eec1a649eea77a7868d32
Gomathy <gomathy1023@gmail.com>
2024-05-31 13:08:03 +0530

    Cart & Wishlist

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -1,189 +1,174 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IonIcon } from "@ionic/react";
-import {
-  closeOutline,
-  addOutline,
-  removeOutline,
-  refreshOutline,
-} from "ionicons/icons";
+import { closeOutline, addOutline, removeOutline, refreshOutline, cartOutline } from "ionicons/icons";
 import { Breadcrumb } from "../../common";
 import "../../styles/cart.css";
+
 const Cart = () => {
+  const [cartItems, setCartItems] = useState([
+    {
+      id: 1,
+      name: "Beige knitted elastic runner shoes",
+      price: 84.00,
+      quantity: 1,
+      image: process.env.PUBLIC_URL + "/assets/home/images/products/clothes-2.jpg",
+    },
+    {
+      id: 2,
+      name: "Blue utility pinafore denim dress",
+      price: 76.00,
+      quantity: 1,
+      image: process.env.PUBLIC_URL + "/assets/home/images/products/clothes-4.jpg",
+    },
+  ]);
+
+  const handleIncrement = (id) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const handleDecrement = (id) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id && item.quantity > 1 ? { ...item, quantity: item.quantity - 1 } : item
+      )
+    );
+  };
+
+  const handleRemove = (id) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
+  };
+
   return (
     <div className="page-content">
       <Breadcrumb />
       <div className="cart">
         <div className="container">
           <div className="row">
-            <div className="col-lg-9">
-              <table className="table table-cart table-mobile">
-                <thead>
-                  <tr>
-                    <th>Product</th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="product-col">
-                      <div className="product">
-                        <figure className="product-media">
-                          <Link to="#">
-                            <img
-                              src={
-                                process.env.PUBLIC_URL +
-                                "/assets/home/images/products/clothes-2.jpg"
-                              }
-                              alt="Product"
-                            />
-                          </Link>
-                        </figure>
-                        <h3 className="product-title">
-                          <Link to="#">Beige knitted elastic runner shoes</Link>
-                        </h3>
-                      </div>
-                    </td>
-                    <td className="price-col">$84.00</td>
-                    <td className="quantity-col">
-                      <div className="cart-product-quantity">
-                        <div className="input-group input-spinner">
-                          <div className="input-group-prepend">
-                            <button
-                              style={{ minWidth: 26 }}
-                              className="btn btn-decrement btn-spinner"
-                              type="button"
-                            >
-                              <IonIcon icon={removeOutline} />
+            <div className={cartItems.length > 0 ? "col-lg-9" : "col-lg-12"}>
+              {cartItems.length > 0 ? (
+                <>
+                  <table className="table table-cart table-mobile">
+                    <thead>
+                      <tr>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {cartItems.map((item) => (
+                        <tr key={item.id}>
+                          <td className="product-col">
+                            <div className="product">
+                              <figure className="product-media">
+                                <Link to="#">
+                                  <img src={item.image} alt="Product" />
+                                </Link>
+                              </figure>
+                              <h3 className="product-title">
+                                <Link to="#">{item.name}</Link>
+                              </h3>
+                            </div>
+                          </td>
+                          <td className="price-col">${item.price.toFixed(2)}</td>
+                          <td className="quantity-col">
+                            <div className="cart-product-quantity">
+                              <div className="input-group input-spinner">
+                                <div className="input-group-prepend">
+                                  <button
+                                    style={{ minWidth: 26 }}
+                                    className="btn btn-decrement btn-spinner"
+                                    type="button"
+                                    onClick={() => handleDecrement(item.id)}
+                                  >
+                                    <IonIcon icon={removeOutline} />
+                                  </button>
+                                </div>
+                                <input
+                                  type="text"
+                                  style={{ textAlign: "center" }}
+                                  className="form-control"
+                                  value={item.quantity}
+                                  readOnly
+                                />
+                                <div className="input-group-append">
+                                  <button
+                                    style={{ minWidth: 26 }}
+                                    className="btn btn-increment btn-spinner"
+                                    type="button"
+                                    onClick={() => handleIncrement(item.id)}
+                                  >
+                                    <IonIcon icon={addOutline} />
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="total-col">${(item.price * item.quantity).toFixed(2)}</td>
+                          <td className="remove-col">
+                            <button className="btn-remove" onClick={() => handleRemove(item.id)}>
+                              <IonIcon icon={closeOutline} />
                             </button>
-                          </div>
-                          <input
-                            type="text"
-                            style={{ textAlign: "center" }}
-                            className="form-control"
-                            required
-                            placeholder=""
-                          />
-                          <div className="input-group-append">
-                            <button
-                              style={{ minWidth: 26 }}
-                              className="btn btn-increment btn-spinner"
-                              type="button"
-                            >
-                              <IonIcon icon={addOutline} />
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="total-col">$84.00</td>
-                    <td className="remove-col">
-                      <button className="btn-remove">
-                        <IonIcon icon={closeOutline} />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="product-col">
-                      <div className="product">
-                        <figure className="product-media">
-                          <Link to="#">
-                            <img
-                              src={
-                                process.env.PUBLIC_URL +
-                                "/assets/home/images/products/clothes-4.jpg"
-                              }
-                              alt="Product"
-                            />
-                          </Link>
-                        </figure>
-                        <h3 className="product-title">
-                          <Link to="#">Blue utility pinafore denim dress</Link>
-                        </h3>
-                      </div>
-                    </td>
-                    <td className="price-col">$76.00</td>
-                    <td className="quantity-col">
-                      <div className="cart-product-quantity">
-                        <div className="input-group input-spinner">
-                          <div className="input-group-prepend">
-                            <button
-                              style={{ minWidth: 26 }}
-                              className="btn btn-decrement btn-spinner"
-                              type="button"
-                            >
-                              <IonIcon icon={removeOutline} />
-                            </button>
-                          </div>
-                          <input
-                            type="text"
-                            style={{ textAlign: "center" }}
-                            className="form-control"
-                            required
-                            placeholder=""
-                          />
-                          <div className="input-group-append">
-                            <button
-                              style={{ minWidth: 26 }}
-                              className="btn btn-increment btn-spinner"
-                              type="button"
-                            >
-                              <IonIcon icon={addOutline} />
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="total-col">$76.00</td>
-                    <td className="remove-col">
-                      <button className="btn-remove">
-                        <IonIcon icon={closeOutline} />
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="cart-bottom">
-                <Link to="#" className="btn btn-outline-dark-2">
-                  <span>UPDATE CART</span>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  <div className="cart-bottom">
+                    <Link to="#" className="btn btn-outline-dark-2">
+                      <span>UPDATE CART</span>
+                      <IonIcon icon={refreshOutline} />
+                    </Link>
+                  </div>
+                </>
+              ) : (
+                <div className="empty-cart">
+                  <IonIcon icon={cartOutline} size="large" />
+                  <p>No items in the cart</p>
+                  <Link to="/" className="continue-shopping btn btn-outline-dark-2 btn-block mb-3">
+                    <span>CONTINUE SHOPPING</span>
+                    <IonIcon icon={refreshOutline} />
+                  </Link>
+                </div>
+              )}
+            </div>
+            {cartItems.length > 0 && (
+              <aside className="col-lg-3">
+                <div className="summary summary-cart">
+                  <h3 className="summary-title">Cart Total</h3>
+                  <table className="table table-summary">
+                    <tbody>
+                      <tr className="summary-subtotal">
+                        <td>Subtotal:</td>
+                        <td>${calculateTotal()}</td>
+                      </tr>
+                      <tr className="summary-total">
+                        <td>Total:</td>
+                        <td>${calculateTotal()}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <Link to="checkout.html" className="btn btn-outline-primary-2 btn-order btn-block">
+                    PROCEED TO CHECKOUT
+                  </Link>
+                </div>
+                <Link to="/" className="btn btn-outline-dark-2 btn-block mb-3">
+                  <span>CONTINUE SHOPPING</span>
                   <IonIcon icon={refreshOutline} />
                 </Link>
-              </div>
-            </div>
-            <aside className="col-lg-3">
-              <div className="summary summary-cart">
-                <h3 className="summary-title">Cart Total</h3>
-                <table className="table table-summary">
-                  <tbody>
-                    <tr className="summary-subtotal">
-                      <td>Subtotal:</td>
-                      <td>$160.00</td>
-                    </tr>
-
-                    <tr className="summary-total">
-                      <td>Total:</td>
-                      <td>$160.00</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <Link
-                  to="checkout.html"
-                  className="btn btn-outline-primary-2 btn-order btn-block"
-                >
-                  PROCEED TO CHECKOUT
-                </Link>
-              </div>
-              <Link
-                to="category.html"
-                className="btn btn-outline-dark-2 btn-block mb-3"
-              >
-                <span>CONTINUE SHOPPING</span>
-                <IonIcon icon={refreshOutline} />
-              </Link>
-            </aside>
+              </aside>
+            )}
           </div>
         </div>
       </div>
--- src/components/Wishlist/wishlist.jsx
+++ src/components/Wishlist/wishlist.jsx
@@ -1,101 +1,146 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IonIcon } from '@ionic/react';
-import { Breadcrumb } from '../../common'
-import { cartOutline, closeOutline, logoFacebook, logoTwitter, logoInstagram, logoYoutube, logoWhatsapp } from 'ionicons/icons';
-import '../../styles/wishlist.css'
+import { Breadcrumb } from '../../common';
+import {
+    cartOutline,
+    closeOutline,
+    logoFacebook,
+    logoTwitter,
+    logoInstagram,
+    logoYoutube,
+    logoWhatsapp,
+    heartDislikeOutline,refreshOutline
+} from 'ionicons/icons';
+import '../../styles/wishlist.css';
+
 const Wishlist = () => {
-    const wishlistItems = [
+    const [wishlistItems, setWishlistItems] = useState([
         {
             id: 1,
-            image: process.env.PUBLIC_URL + "/assets/home/images/products/clothes-2.jpg",
+            image: process.env.PUBLIC_URL + '/assets/home/images/products/clothes-2.jpg',
             title: 'Beige knitted elastic runner shoes',
-            price: 84.00,
+            price: 84.0,
             stockStatus: 'In stock',
         },
         {
             id: 2,
-            image: process.env.PUBLIC_URL + "/assets/home/images/products/clothes-3.jpg",
+            image: process.env.PUBLIC_URL + '/assets/home/images/products/clothes-3.jpg',
             title: 'Blue utility pinafore denim dress',
-            price: 76.00,
+            price: 76.0,
             stockStatus: 'In stock',
         },
         {
             id: 3,
-            image: process.env.PUBLIC_URL + "/assets/home/images/products/clothes-4.jpg",
+            image: process.env.PUBLIC_URL + '/assets/home/images/products/clothes-4.jpg',
             title: 'Orange saddle lock front chain cross body bag',
-            price: 52.00,
+            price: 52.0,
             stockStatus: 'Out of stock',
         },
-    ];
+    ]);
+
+    const handleAddToCart = (id) => {
+        // Implement your logic to add the item to the cart here
+        console.log(`Item with id ${id} added to cart`);
+    };
+
+    const handleRemove = (id) => {
+        setWishlistItems(wishlistItems.filter((item) => item.id !== id));
+    };
 
     return (
-        <div className="page-content">
+        <div className="cart page-content">
             <Breadcrumb />
             <div className="container">
-                <table className="table table-wishlist table-mobile">
-                    <thead>
-                        <tr>
-                            <th>Product</th>
-                            <th>Price</th>
-                            <th>Stock Status</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {wishlistItems.map((item) => (
-                            <tr key={item.id}>
-                                <td className="product-col">
-                                    <div className="product">
-                                        <figure className="product-media">
-                                            <Link to="#">
-                                                <img src={item.image} alt="Product" />
-                                            </Link>
-                                        </figure>
-                                        <h3 className="product-title">
-                                            <Link to="#">{item.title}</Link>
-                                        </h3>
-                                    </div>
-                                </td>
-                                <td className="price-col">${item.price.toFixed(2)}</td>
-                                <td className="stock-col">
-                                    <span className={item.stockStatus === 'In stock' ? 'in-stock' : 'out-of-stock'}>
-                                        {item.stockStatus}
-                                    </span>
-                                </td>
-                                <td className="action-col">
-                                    {item.stockStatus === 'In stock' ? (
-                                        <button className="btn btn-cart btn-block btn-outline-primary-2">
-                                            <IonIcon icon={cartOutline} /> Add to Cart
-                                        </button>
-                                    ) : (
-                                        <button className="btn btn-cart btn-block btn-outline-primary-2 disabled">
-                                            Out of Stock
-                                        </button>
-                                    )}
-                                </td>
-                                <td className="remove-col">
-                                    <button className="btn-remove">
-                                        <IonIcon icon={closeOutline} />
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <div className="wishlist-share">
-                    <div className="social-icons social-icons-sm mb-2">
-                        <label className="social-label">Share on:</label>
-                        <div className='social-links'>
-                            <Link to="#" className="social-icon" title="Facebook" target="_blank"><IonIcon icon={logoFacebook} /></Link>
-                            <Link to="#" className="social-icon" title="Twitter" target="_blank"><IonIcon icon={logoTwitter} /></Link>
-                            <Link to="#" className="social-icon" title="Instagram" target="_blank"><IonIcon icon={logoInstagram} /></Link>
-                            <Link to="#" className="social-icon" title="YouTube" target="_blank"><IonIcon icon={logoYoutube} /></Link>
-                            <Link to="#" className="social-icon" title="WhatsApp" target="_blank"><IonIcon icon={logoWhatsapp} /></Link>
+                {wishlistItems.length > 0 ? (
+                    <>
+                        <table className="table table-wishlist table-mobile">
+                            <thead>
+                                <tr>
+                                    <th>Product</th>
+                                    <th>Price</th>
+                                    <th>Stock Status</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {wishlistItems.map((item) => (
+                                    <tr key={item.id}>
+                                        <td className="product-col">
+                                            <div className="product">
+                                                <figure className="product-media">
+                                                    <Link to="#">
+                                                        <img src={item.image} alt="Product" />
+                                                    </Link>
+                                                </figure>
+                                                <h3 className="product-title">
+                                                    <Link to="#">{item.title}</Link>
+                                                </h3>
+                                            </div>
+                                        </td>
+                                        <td className="price-col">${item.price.toFixed(2)}</td>
+                                        <td className="stock-col">
+                                            <span className={item.stockStatus === 'In stock' ? 'in-stock' : 'out-of-stock'}>
+                                                {item.stockStatus}
+                                            </span>
+                                        </td>
+                                        <td className="action-col">
+                                            {item.stockStatus === 'In stock' ? (
+                                                <button
+                                                    className="btn btn-cart btn-block btn-outline-primary-2"
+                                                    onClick={() => handleAddToCart(item.id)}
+                                                >
+                                                    <IonIcon icon={cartOutline} /> Add to Cart
+                                                </button>
+                                            ) : (
+                                                <button className="btn btn-cart btn-block btn-outline-primary-2 disabled">
+                                                    Out of Stock
+                                                </button>
+                                            )}
+                                        </td>
+                                        <td className="remove-col">
+                                            <button className="btn-remove" onClick={() => handleRemove(item.id)}>
+                                                <IonIcon icon={closeOutline} />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div className="wishlist-share">
+                            <div className="social-icons social-icons-sm mb-2">
+                                <label className="social-label">Share on:</label>
+                                <div className="social-links">
+                                    <Link to="#" className="social-icon" title="Facebook" target="_blank">
+                                        <IonIcon icon={logoFacebook} />
+                                    </Link>
+                                    <Link to="#" className="social-icon" title="Twitter" target="_blank">
+                                        <IonIcon icon={logoTwitter} />
+                                    </Link>
+                                    <Link to="#" className="social-icon" title="Instagram" target="_blank">
+                                        <IonIcon icon={logoInstagram} />
+                                    </Link>
+                                    <Link to="#" className="social-icon" title="YouTube" target="_blank">
+                                        <IonIcon icon={logoYoutube} />
+                                    </Link>
+                                    <Link to="#" className="social-icon" title="WhatsApp" target="_blank">
+                                        <IonIcon icon={logoWhatsapp} />
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
+                    </>
+                ) : (
+                    <div className="empty-cart">
+                        <IonIcon icon={heartDislikeOutline} size="large" />
+                        <p>No items in your wishlist</p>
+                        <Link to="/" className="continue-shopping btn btn-outline-dark-2 btn-block mb-3">
+                            <span>CONTINUE SHOPPING</span>
+                            <IonIcon icon={refreshOutline} />
+                        </Link>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
