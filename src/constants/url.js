2b5e3410fedb46e5f15932e3f3c9158e5b362a8d
Gomathy <gomathy1023@gmail.com>
2024-06-05 18:35:20 +0530

    URL Change

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,8 +1,8 @@
 // const BACKEND_API_URL = "https://modenteo-backend.onrender.com/api.modenteo/v1";
-// const BACKEND_API_URL = "https://api-modenteo.onrender.com/v1";
+const BACKEND_API_URL = "https://api-modenteo.onrender.com/v1";
 const BACKEND_BASE_URL = "https://api-modenteo.onrender.com/";
 const BACKEND_IMG_URL = "https://api-modenteo.onrender.com";
 const BASE_URL = "https://dev-modenteo.vercel.app/";
 // const BASE_URL = "http://localhost:3001/";
-const BACKEND_API_URL = "http://localhost:3000/v1";
+// const BACKEND_API_URL = "http://localhost:3000/v1";
 export { BACKEND_API_URL, BACKEND_BASE_URL, BASE_URL, BACKEND_IMG_URL };
