88826ce719f74ad9d878550fac3319b4d9e4f31f
Gomathy <gomathy1023@gmail.com>
2024-06-05 11:41:56 +0530

    Add to cart

--- src/constants/url.js
+++ src/constants/url.js
@@ -2,7 +2,7 @@
 const BACKEND_API_URL = "https://api-modenteo.onrender.com/v1";
 const BACKEND_BASE_URL = "https://api-modenteo.onrender.com/";
 const BACKEND_IMG_URL = "https://api-modenteo.onrender.com";
-// const BASE_URL = "https://dev-modenteo.vercel.app/";
-const BASE_URL = "http://localhost:3001/";
+const BASE_URL = "https://dev-modenteo.vercel.app/";
+// const BASE_URL = "http://localhost:3001/";
 // const BACKEND_API_URL = "http://localhost:3000/v1";
 export { BACKEND_API_URL, BACKEND_BASE_URL, BASE_URL, BACKEND_IMG_URL };
