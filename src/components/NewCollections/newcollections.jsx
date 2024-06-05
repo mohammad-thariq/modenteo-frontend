3b5aea832e412e99b4afc0efc39d0c9d43fd8935
mohammad-thariq <thariq.dev@gmail.com>
2024-06-05 11:27:31 +0530

    Fix Eslint Error

--- src/components/NewCollections/newcollections.jsx
+++ src/components/NewCollections/newcollections.jsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../styles/category.css';
-import { ManageCategoriesApi } from '../../service';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/category.css";
+import { ManageCategoriesApi } from "../../service";
 import { useQuery } from "react-query";
-import { BASE_URL, BACKEND_IMG_URL } from '../../constants/url';
-import { Loading, NoRecordFound } from '../../common';
-import { useParams } from 'react-router-dom';
-import { getNextJsOptimizedUrl } from '../../helper/image';
+import { BASE_URL } from "../../constants/url";
+import { Loading, NoRecordFound } from "../../common";
+// import { useParams } from "react-router-dom";
 const { productMenuNew } = new ManageCategoriesApi();
 
 const NewCollections = () => {
-    const { id } = useParams();
-    const { data, isLoading } = useQuery('menu-new-collections', productMenuNew);
+  //   const { id } = useParams();
+  const { data, isLoading } = useQuery("menu-new-collections", productMenuNew);
 
-    if (isLoading) {
-        return <Loading />;
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    // Ensure data is not null or undefined and contains the 'categories' property
-    if (!data || !data.response) {
-        return <NoRecordFound />;
-    }
+  // Ensure data is not null or undefined and contains the 'categories' property
+  if (!data || !data.response) {
+    return <NoRecordFound />;
+  }
 
-    return (
-        <section className="cat-outer-section">
-            <div className="container">
-                <div className="row">
-                    {data.response.map(item => (
-                        <div key={item.slug} className="col-sm-6 col-md-6 col-lg-4">
-                            <div className="category-section-blocks">
-                                <Link to={BASE_URL + "new/" + item.slug} className="cat-list-img">
-                                    <img src={item.image} alt={item.name} />
-                                </Link>
-                                <Link to={BASE_URL + "new/" + item.slug}>{item.name}</Link>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <section className="cat-outer-section">
+      <div className="container">
+        <div className="row">
+          {data.response.map((item) => (
+            <div key={item.slug} className="col-sm-6 col-md-6 col-lg-4">
+              <div className="category-section-blocks">
+                <Link
+                  to={BASE_URL + "new/" + item.slug}
+                  className="cat-list-img"
+                >
+                  <img src={item.image} alt={item.name} />
+                </Link>
+                <Link to={BASE_URL + "new/" + item.slug}>{item.name}</Link>
+              </div>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NewCollections;
